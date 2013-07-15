ce5c65d5a5c741a236e57e72031c7665ea9decea
Aaron Kable <aaronkable@gmail.com>
2013-07-15 23:38:29 +0800

    added basic Nav Draw Handling (Very Basic)

--- src/com/pac/console/PacConsole.java
+++ src/com/pac/console/PacConsole.java
@@ -1,15 +1,76 @@
 package com.pac.console;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class PacConsole extends Activity {
+
+    private ArrayList<String> mGameTitles;
+    private ListView mDrawerList;
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pac_console);
+        
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  
+                mDrawerLayout,         
+                R.drawable.ic_drawer, 
+                R.string.app_name, 
+                R.string.app_name  
+                ) {
+        	
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+            	//TODO Update the actionbar title
+            	//getActionBar().setTitle("Closed");
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+            	//TODO Update the actionbar title
+            	//getActionBar().setTitle("Open");
+
+            }
+        };
+        
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        
+        // TODO use proper adapters and shit
+        mGameTitles = new ArrayList<String>();
+        mGameTitles.add("One");
+        mGameTitles.add("Two");
+        mGameTitles.add("Three");
+        mGameTitles.add("Four");
+        mGameTitles.add("Five");
+
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mGameTitles));
+        
+        
+        // setup the drawer tab
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
     }
 
 
@@ -19,5 +80,17 @@ public class PacConsole extends Activity {
         getMenuInflater().inflate(R.menu.pac_console, menu);
         return true;
     }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+    	if (mDrawerToggle.onOptionsItemSelected(item)) {
+          return true;
+        }
+        // TODO Handle your other itmes...
+
+        return super.onOptionsItemSelected(item);
+    }
+
     
 }
