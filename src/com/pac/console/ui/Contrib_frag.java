deb46923b2f3007f54b1ac70842207e746d98dfe
Aaron Kable <aaronkable@gmail.com>
2013-07-31 15:38:32 +0800

    contrib tab edits

--- src/com/pac/console/ui/Contrib_frag.java
+++ src/com/pac/console/ui/Contrib_frag.java
@@ -1,6 +1,12 @@
 package com.pac.console.ui;
 
+import com.pac.console.R;
+
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class Contrib_frag extends Fragment {
 	
@@ -9,6 +15,16 @@ public class Contrib_frag extends Fragment {
 		Contrib_frag fragment = new Contrib_frag();		
 		return fragment;
 	
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle oflove) {
+		
+		View layout = inflater.inflate(R.layout.contrib_frag_layout, null);
+
+		
+		
+		return layout;
 	}
 
 }
