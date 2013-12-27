8ea5963d6486b620e32045d894fa9298c6958f94
Aaron Kable <aaronkable@gmail.com>
2013-12-27 17:39:49 +0800

    fix mkfile

--- Android.mk
+++ Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files) $(call all-renderscript-files-under, src)
 
